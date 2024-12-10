09bb5f8b965883375428e8449e4e85b21c90d48f
Kody Kapetanis <kodykapetanis@yahoo.com>
2024-12-10 05:55:47 -0500

    3.6 Bootstrap responsive grid

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,30 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap'; // Correctly import Container and Row
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 import { MainView } from './components/main-view';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   return (
-    <container>
-    <div className="my-flix">
-      <row>
-        <col xs={12} md={4} className="bg-primary text-white p-3">
-        column 1
-        </col>
-        <Col xs={12} md={4} className="bg-secondary text-white p-3">
-          Column 2
-        </Col>
-        <Col xs={12} md={4} className="bg-success text-white p-3">
-          Column 3
-        </Col>
-      </row>
-      <MainView />
-    </div>
-    </container>
+    <Container>
+      <div className="my-flix">
+        <Row>
+          <Col xs={12} md={4} className="bg-primary text-white p-3">
+            Column 1
+          </Col>
+          <Col xs={12} md={4} className="bg-secondary text-white p-3">
+            Column 2
+          </Col>
+          <Col xs={12} md={4} className="bg-success text-white p-3">
+            Column 3
+          </Col>
+        </Row>
+        <MainView />
+      </div>
+    </Container>
   );
 };
 
